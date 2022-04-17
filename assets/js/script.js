cf97850acbf3815c907c7dce7722cbe359883e37
Peterson <petersonros@hotmail.com>
2022-04-16 21:08:08 -0300

    Apagando linhas desnecessárias de código de JavaScript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,30 @@
+// Criptografar
+// A vogal "a" é trocada por "ai"
+// A vogal "e" é trocada por "enter"
+// A vogal "i" é trocada por "imes"
+// A vogal "o" é trocada por "ober"
+// A vogal "u" é trocada por "ufat"
 
-var initialText = document.querySelector("#input") ;
-var clickEncoded = document.querySelector("#encoded");
-var clickDecoded = document.querySelector("#decoded");
-var textResult;
-var encoded;
-var decoded;  
-
-// Codificador
-/*
-A vogal "a" é trocada por "ai"
-A vogal "e" é trocada por "enter"
-A vogal "i" é trocada por "imes"
-A vogal "o" é trocada por "ober"
-A vogal "u" é trocada por "ufat"
-*/
+document.querySelector("#encoded").onclick = function(){textEncoded();}
 function textEncoded(){
-        var myFrase = initialText.value;             
+        var myFrase = document.querySelector("#input").value;             
         encoded = myFrase.replace(/a/g, 'ai').replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/o/g, 'ober').replace(/u/g, 'ufat');
-        textResult = encoded;
         document.querySelector('#init-text').value= encoded;
-        }
+}
 
-//Decodificador
-/*
-"ai"     é trocado pela vogal "a"
-"enter"  é trocado pela vogal "e"
-"imes"   é trocado pela vogal "i"
-"ober"   é trocado pela vogal "o"
-"ufat"   é trocado pela vogal "u"
-*/
+// Descriptografar
+// "ai"     é trocado pela vogal "a"
+// "enter"  é trocado pela vogal "e"
+// "imes"   é trocado pela vogal "i"
+// "ober"   é trocado pela vogal "o"
+// "ufat"   é trocado pela vogal "u"
+
+document.querySelector("#decoded").onclick = function(){textDecoded();}
 function textDecoded(){
-        var myFrase = initialText.value;
+        var myFrase = document.querySelector("#input").value;
         decoded = myFrase.replace(/ai/g, 'a').replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u');
-        textResult = decoded;
         document.querySelector('#init-text').value= decoded;
-        }
+}
 
-clickEncoded.onclick = function(){textEncoded();}
-
-clickDecoded.onclick = function(){textDecoded();}
-
-function copyText(){
-        navigator.clipboard.writeText(encoded);
-    }
+function copyText(){navigator.clipboard.writeText(encoded);}
 
